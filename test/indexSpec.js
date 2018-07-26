75a062c1ffacc5cd9f0622a9e400e63c092b6e18
David Núñez <david.nunez@bayteq.com>
2018-07-26 13:22:26 -0500

    fix test and add new tests

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -2,53 +2,58 @@
 /**
  * Created by Davip on 07/01/2015.
  */
-
-'use strict';
 const _ = require('underscore');
 const assert  = require('chai').assert;
 const include   = require('..');
   
-describe('There are not files nested', function(){
+describe('There are not files nested', () => {
     let modules;
 
-    before(function(){
+    before(() => {
 
         // relative path of a empty folder
         const paths = {
             'empty':'./examples/empty'
         }
         
-        modules = include.modules(paths, __dirname);
+        modules = include.modules(paths, __dirname, "");
     });
 
-    describe('should be a object', function(){    
-        it('should be a object', function(){
-            assert.isObject(modules, 'is an object');
+    describe('should be an object', () => {    
+        it('should be an object', () => {
+            assert.isObject(modules, 'it is an object');
         });
     });
 
-    describe('should be a null object', function(){
-        it('should be a null object', function(){
-            assert.equal(_.size(modules), 0);
+    describe('should be a null object', () => {
+        it('should be a null object', () => {
+            assert.equal(_.size(modules.empty), 0);
         });
-    });
-    
-    
+    })
 });
 
-describe('There are files nested', function(){
+describe('There are files nested', () => {
 
-    describe('should be the same name the name property defined in paths variable and result object', function(){
+    describe('should be the same name and the name property defined in paths variable and result object', () => {
 
-        it('should be the same name', function(){
+        it('should be the same name', () => {
             const paths = {
                 'development':'./examples/development',
                 'default':'./examples/default'
             };
-            const modules = include.modules(paths, __dirname);            
+            const modules = include.modules(paths, __dirname, "");
             assert.equal(Object.getOwnPropertyNames(paths)[0], Object.getOwnPropertyNames(modules)[0]);
-            assert.equal(Object.getOwnPropertyNames(paths)[1], Object.getOwnPropertyNames(modules)[1]);
         });
     });
-
 });
+
+describe('Load just an unique file', () => {
+    it('should find an specified file in an specified path', () => {
+        const paths = {
+            'single': './examples/single'
+        };
+        const modules = include.modules(paths,__dirname, 'example.json')
+        assert.equal(Object.getOwnPropertyNames(paths)[0], Object.getOwnPropertyNames(modules)[0]);
+        
+    })
+})
